34b4508ced262ed76c1157cc8a2ffe7eaf83584e
Kalyl Henings <kalylhenings@gmail.com>
2019-11-15 01:54:03 -0300

    4

--- lista2/lista2.hs
+++ lista2/lista2.hs
@@ -10,7 +10,9 @@ inverte (x:xs) = inverte xs ++ [x]
 
 semListaVazia x = inverte $ foldl (\acc x -> if length x > 0 then x:acc else acc) [] x
 --ex4
-
+cNroPar [] = []
+cNroPar (x:xs) = semListaVazia $ checaPar x : cNroPar xs
+    where checaPar x = if length (filter even x) > 0 then x else [] 
 --ex5
 
 --ex6
