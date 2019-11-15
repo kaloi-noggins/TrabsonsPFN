4b436c6658795693abcb752dedb216d0a29fae86
Kalyl Henings <kalylhenings@gmail.com>
2019-11-15 02:27:40 -0300

    5 fodifo

--- lista2/lista2.hs
+++ lista2/lista2.hs
@@ -1,8 +1,10 @@
+{-Alunos: Danilo Sousa Ruiz dos Passos e Kalyl Henings-}
+
 --ex1
-reversoAninhado x = map reverse x
+reversoAninhado xs = map reverse xs
 
 --ex2
-aFrente n x = map (n:) x
+aFrente n xs = map (n:) xs
 
 --ex3
 inverte [] = []
@@ -13,7 +15,13 @@ semListaVazia x = inverte $ foldl (\acc x -> if length x > 0 then x:acc else acc
 cNroPar [] = []
 cNroPar (x:xs) = semListaVazia $ checaPar x : cNroPar xs
     where checaPar x = if length (filter even x) > 0 then x else [] 
---ex5
+
+    --ex5
+addNmb _ [] = []
+addNmb n (x:xs) = n++x : addNmb n xs
+
+aFrentePar n xs = addNmb n listaListaPares
+    where listaListaPares = cNroPar xs
 
 --ex6
 
